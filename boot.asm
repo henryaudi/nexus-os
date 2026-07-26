dad015bc1fb52edb94377f0de3dc43de70dd80f0
Shangjie Zheng <henryperfectest@gmail.com>
2026-07-26 13:34:43 -0600

    Enable boot to enter protected mode

--- boot.asm
+++ boot.asm
@@ -1,5 +1,9 @@
-ORG  0
+ORG  0x7C00
 BITS 16
+
+CODE_SEG equ gdt_code - gdt_start
+DATA_SEG equ gdt_data - gdt_start
+
 _start:
     jmp short start
     nop
@@ -7,58 +11,58 @@ _start:
 times 33 db 0
 
 start:
-    jmp 0x7C0:step2
-
-
+    jmp 0:step2
 
 step2:
     cli 
-    mov ax, 0x7C0
+    mov ax, 0x00
     mov ds, ax
     mov es, ax
-    mov ax, 0x00
     mov ss, ax
     mov sp, 0x7C00
     sti 
 
-    mov ah, 2 
-    mov al, 1
-    mov ch, 0
-    mov cl, 2
-    mov dh, 0
-    mov bx, buffer
-    int 0x13
-    jc  error
+.load_protected:
+    cli
+    lgdt[gdt_descriptor]
+    mov eax, cr0
+    or  eax, 0x1
+    mov cr0, eax
+    jmp CODE_SEG:load32
 
-    mov si, buffer
-    call print
+; GDT
+gdt_start:
+gdt_null:
+    dd 0x0
+    dd 0x0
 
+; offset 0x8
+gdt_code:        ; CS SHOULD PRINT TO THIS
+    dw 0xFFFF    ; Segment limit first 0-15 bits
+    dw 0         ; Base first 0-15 bits
+    db 0         ; Base 16-23 bits
+    db 0x9A      ; Access byte
+    db 11001111b ; High 4 bit flags and the low 4 bit flags
+    db 0         ; Base 24-31 bits
+
+; offset 0x10
+gdt_data:        ; Link to DS, SS, ES, FS, GS
+    dw 0xFFFF    ; Segment limit first 0-15 bits
+    dw 0         ; Base first 0-15 bits
+    db 0         ; Base 16-23 bits
+    db 0x92      ; Access byte
+    db 11001111b ; High 4 bit flags and the low 4 bit flags
+    db 0         ; Base 24-31 bits
+
+gdt_end:
+
+gdt_descriptor:
+    dw gdt_end - gdt_start - 1
+    dd gdt_start
+
+[BITS 32]
+load32:
     jmp $
-
-error:
-    mov si, error_message
-    call print
-    jmp $
-
-print:
-    mov bx, 0
-.loop:
-    lodsb
-    cmp al, 0
-    je .done
-    call print_char
-    jmp .loop
-.done:
-    ret
-
-print_char:
-    mov ah, 0eh
-    int 0x10
-    ret
-
-error_message: db 'Failed to load sector', 0
 
 times 510- ($ - $$) db 0
 dw 0xAA55
-
-buffer:

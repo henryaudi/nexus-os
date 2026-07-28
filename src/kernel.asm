4f20f410d5fa006c026771fd03a332984f5c7d9a
Shangjie Zheng <henryperfectest@gmail.com>
2026-07-27 23:44:56 -0600

    Add C kernel entry point

--- src/kernel.asm
+++ src/kernel.asm
@@ -1,6 +1,6 @@
 [BITS 32]
 global _start
-
+extern kernel_main
 CODE_SEG equ 0x08
 DATA_SEG equ 0x10
 
@@ -18,6 +18,8 @@ _start:
     in  al,   0x92
     or  al,   2
     out 0x92, al
+
+    call kernel_main
 
     jmp $
 

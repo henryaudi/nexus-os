d5ae4a62b29c1184e912383550b6385d878f5dae
Shangjie Zheng <henryperfectest@gmail.com>
2026-07-31 15:28:25 -0600

    Move interrupt enable to idt module

--- src/kernel.c
+++ src/kernel.c
@@ -80,6 +80,9 @@ void kernel_main()
     // Initialize the Interrupt Descriptor Table (IDT)
     idt_init();
 
+    // Enable interrupts
+    enable_interrupts();
+
     void *ptr  = kmalloc(50);
     void *ptr2 = kmalloc(5000);
     void *ptr3 = kmalloc(5600);

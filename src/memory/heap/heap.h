e0f65b9565e1580d2b2340e86f50ca7e1d01775a
Shangjie Zheng <henryperfectest@gmail.com>
2026-07-31 09:53:06 -0600

    Implmented heap_malloc

--- src/memory/heap/heap.h
+++ src/memory/heap/heap.h
@@ -6,10 +6,10 @@
 #include <stddef.h>
 
 #define HEAP_BLOCK_TABLE_ENTRY_TAKEN 0x01
-#define HEAP_BLOCK_TABLE_ENTRY_FREE  0x00
+#define HEAP_BLOCK_TABLE_ENTRY_FREE 0x00
 
 #define HEAP_BLOCK_HAS_NEXT 0b10000000
-#define HEAP_BLOCK_IS_FREE  0b01000000
+#define HEAP_BLOCK_IS_FIRST 0b01000000
 
 typedef unsigned char HEAP_BLOCK_TABLE_ENTRY;
 
@@ -22,9 +22,13 @@ struct heap_table
 struct heap
 {
     struct heap_table *table;
-    void              *saddr;  // Start address of the heap data pool
+    void              *saddr; // Start address of the heap data pool
 };
 
 int heap_create(struct heap *heap, void *ptr, void *end, struct heap_table *table);
+
+void *heap_malloc(struct heap *heap, size_t size);
+
+void heap_free(struct heap *heap, void *ptr);
 
 #endif

1147b8c26044310fbd0a38e25a8ef73fa8b6132e
Shangjie Zheng <henryperfectest@gmail.com>
2026-08-01 10:46:52 -0600

    Implement paging get indices (paging directory -> paging table, paging table -> physical address block idx)

--- src/memory/paging/paging.c
+++ src/memory/paging/paging.c
@@ -1,5 +1,6 @@
 #include "paging.h"
 #include "memory/heap/kheap.h"
+#include "status.h"
 
 static uint32_t *current_directory = 0;
 
@@ -8,14 +9,16 @@ void paging_load_directory(uint32_t *directory);
 struct paging_4gb_chunk *paging_new_4gb(uint8_t flags)
 {
     uint32_t *directory = kzalloc(PAGING_TOTAL_ENTRIES_PER_TABLE * sizeof(uint32_t));
-    if (!directory) return 0;
+    if (!directory)
+        return 0;
 
     int offset = 0;
 
     for (int i = 0; i < PAGING_TOTAL_ENTRIES_PER_TABLE; i++)
     {
         uint32_t *entry = kzalloc(sizeof(uint32_t) * PAGING_TOTAL_ENTRIES_PER_TABLE);
-        if (!entry) return 0;
+        if (!entry)
+            return 0;
 
         for (int b = 0; b < PAGING_TOTAL_ENTRIES_PER_TABLE; b++)
         {
@@ -27,7 +30,7 @@ struct paging_4gb_chunk *paging_new_4gb(uint8_t flags)
     }
 
     struct paging_4gb_chunk *chunk_4gb = kzalloc(sizeof(struct paging_4gb_chunk));
-    chunk_4gb->directory_entry = directory;
+    chunk_4gb->directory_entry         = directory;
     return chunk_4gb;
 }
 
@@ -40,4 +43,29 @@ void paging_switch(uint32_t *directory)
 uint32_t *paging_4gb_chunk_get_directory(struct paging_4gb_chunk *chunk)
 {
     return chunk->directory_entry;
+}
+
+bool paging_is_aligned(void *addr)
+{
+    return ((uint32_t)addr % PAGING_PAGE_SIZE) == 0;
+}
+
+int paging_get_indexes(void *virtual_address, uint32_t *directory_index_out,
+                       uint32_t *table_index_out)
+{
+    int res = 0;
+
+    if (!paging_is_aligned(virtual_address))
+    {
+        res = -EINVARG;
+        goto out;
+    }
+
+    *directory_index_out =
+        ((uint32_t)virtual_address / (PAGING_TOTAL_ENTRIES_PER_TABLE * PAGING_PAGE_SIZE));
+    *table_index_out =
+        ((uint32_t)virtual_address % (PAGING_TOTAL_ENTRIES_PER_TABLE * PAGING_PAGE_SIZE)) /
+        PAGING_PAGE_SIZE;
+out:
+    return res;
 }

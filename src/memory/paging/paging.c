19e538e0ef530f81f2211977fbd8656bb173866e
Shangjie Zheng <henryperfectest@gmail.com>
2026-08-01 11:47:37 -0600

    Add set paging function for setting the virtual memory

--- src/memory/paging/paging.c
+++ src/memory/paging/paging.c
@@ -69,3 +69,26 @@ int paging_get_indexes(void *virtual_address, uint32_t *directory_index_out,
 out:
     return res;
 }
+
+int paging_set(uint32_t *directory, void *virt, uint32_t val)
+{
+    if (!paging_is_aligned(virt))
+    {
+        return -EINVARG;
+    }
+
+    uint32_t directory_index = 0;
+    uint32_t table_index     = 0;
+    int      res             = paging_get_indexes(virt, &directory_index, &table_index);
+    if (res < 0)
+    {
+        return res;
+    }
+
+    uint32_t  entry = directory[directory_index];
+    // Extract "bits 31-12 of address" from Page Directory Entry to its PTE address
+    uint32_t *table = (uint32_t *)(entry & 0xFFFFF000);
+    table[table_index] = val;
+
+    return 0;
+}
--- src/memory/paging/paging.h
+++ src/memory/paging/paging.h
@@ -23,5 +23,6 @@ uint32_t                *paging_4gb_chunk_get_directory(struct paging_4gb_chunk 
 struct paging_4gb_chunk *paging_new_4gb(uint8_t flags);
 void                     paging_switch(uint32_t *directory);
 void                     enable_paging();
-
+int                      paging_set(uint32_t *directory, void *virt, uint32_t val);
+bool                     paging_is_aligned(void *addr);
 #endif

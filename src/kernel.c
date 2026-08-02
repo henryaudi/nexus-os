3e66ec01f435c2a7fe1c4b1588a93c74569c4f64
Shangjie Zheng <henryperfectest@gmail.com>
2026-08-02 15:56:19 -0600

    Implement pathparser functions, expose pathparser_path

--- src/kernel.c
+++ src/kernel.c
@@ -3,9 +3,11 @@
 #include "kernel.h"
 #include "idt/idt.h"
 #include "io/io.h"
+#include "string/string.h"
 #include "memory/heap/kheap.h"
 #include "memory/paging/paging.h"
 #include "disk/disk.h"
+#include "fs/pparser.h"
 
 uint16_t *video_mem    = 0;
 uint16_t  terminal_row = 0;
@@ -51,17 +53,6 @@ void terminal_initialize()
     }
 }
 
-size_t strlen(const char *str)
-{
-    size_t len = 0;
-    while (str[len])
-    {
-        len++;
-    }
-
-    return len;
-}
-
 void print(const char *str)
 {
     size_t len = strlen(str);
@@ -98,4 +89,11 @@ void kernel_main()
     
     // Enable interrupts
     enable_interrupts();
+
+    struct path_root *root_path = pathparser_parse("0:/bin/shell.exe", NULL);
+
+    if (root_path)
+    {
+
+    }
 }
--- src/string/string.h
+++ src/string/string.h
@@ -5,6 +5,6 @@
 int  strlen(const char *ptr);
 bool isdigit(char c);
 int  tonumericdigit(char c);
-int  strnlen(const char *ptr, int max)
+int  strnlen(const char *ptr, int max);
 
 #endif

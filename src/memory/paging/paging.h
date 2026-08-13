80c45935e0e1a23194d300682211fa6fc7ba0e4a
Shangjie Zheng <henryperfectest@gmail.com>
2026-08-13 08:33:44 -0600

    Add method to copy string from task

--- src/memory/paging/paging.h
+++ src/memory/paging/paging.h
@@ -26,8 +26,11 @@ void                     paging_free_4gb(struct paging_4gb_chunk *chunk);
 void                     enable_paging();
 int                      paging_set(uint32_t *directory, void *virt, uint32_t val);
 bool                     paging_is_aligned(void *addr);
-int                      paging_map(struct paging_4gb_chunk *directory, void *virt, void *phys, int flags);
-int   paging_map_range(struct paging_4gb_chunk *directory, void *virt, void *phys, int count, int flags);
-int   paging_map_to(struct paging_4gb_chunk *directory, void *virt, void *phys, void *phys_end, int flags);
-void *paging_align_address(void *ptr);
+int      paging_map(struct paging_4gb_chunk *directory, void *virt, void *phys, int flags);
+int      paging_map_range(struct paging_4gb_chunk *directory, void *virt, void *phys, int count,
+                          int flags);
+int      paging_map_to(struct paging_4gb_chunk *directory, void *virt, void *phys, void *phys_end,
+                       int flags);
+void    *paging_align_address(void *ptr);
+uint32_t paging_get(uint32_t *directory, void *virt);
 #endif
--- src/task/task.h
+++ src/task/task.h
@@ -63,9 +63,9 @@ void task_return(struct registers *regs);
 void restore_general_purpose_registers(struct registers *regs);
 void user_registers();
 
-int  task_switch(struct task *task);
-int  task_page();
+int task_switch(struct task *task);
+int task_page();
 
 void task_current_save_state(struct interrupt_frame *frame);
-
+int  copy_string_from_task(struct task *task, void *virtual, void *phys, int max);
 #endif
